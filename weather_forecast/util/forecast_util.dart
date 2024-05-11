dd2079100500ae709c32896a34d7f6900cc0737f
Spidey47 <168190635+Spidey47@users.noreply.github.com>
2024-05-11 19:53:54 +0530

    Delete weather_forecast directory

--- weather_forecast/util/forecast_util.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class Util{
-  static String appId = "803335b0779e013be70b05c16a91acfb";
-
-  static String getFormattedDate(DateTime dateTime) {
-    return  DateFormat("EEEE, MMM d, y").format(dateTime);
-  }
-}
